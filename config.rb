e021896e796c176daf6fefdbce3d3ef2f9f99746
wootencl <wootencl@g.cofc.edu>
2015-08-13 15:28:04 -0400

    update before launch

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "js"
+css_dir = "app/css"
+sass_dir = "app/scss"
+images_dir = "app/images"
+javascripts_dir = "app/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
